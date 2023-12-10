02408446bce1dfee6040a5ff2681d761e37652c2
penguin-teal <_penguin@tuta.io>
2023-12-10 15:14:41 -0600

    Structure for freeing and compile step

--- src/main.c
+++ src/main.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "lexer.h"
 #include "fileIo.h"
 
 int main(int argc, char **argv)
 {
-    FILE *srcF = fopen("./testcode.ansll", "r");
-    if(!srcF)
-    {
-        fprintf(stderr, "Couldn't open source file.");
-        return 1;
-    }
+    int ret = 0;
+    bool verbose = true;
 
-    size_t fSize = fGetSize(srcF);
-
-    struct Token *tokens;
-    char *strings;
-    if(!lexFile(srcF, fSize, "testcode.ansll", &tokens, &strings))
+    for(int i = 0; i < 1; i++)
     {
-        fprintf(stderr, "Compilation Failed - Lexing failed.");
-        return 1;
+        FILE *srcF = fopen("./testcode.ansll", "r");
+        if(!srcF)
+        {
+            fprintf(stderr, "Couldn't open source file.");
+            return 1;
+        }
+
+        size_t fSize = fGetSize(srcF);
+
+        struct Token *tokens;
+        char *strings;
+        if(!lexFile(srcF, fSize, "testcode.ansll", &tokens, &strings))
+        {
+            fprintf(stderr, "Compilation Failed - Lexing failed.");
+            ret = 1;
+            goto LexingFailed;
+        }
+
+        if(false)
+        {
+            fprintf(stderr, "Compilation Failed - Compiling failed.");
+            ret = 1;
+            goto CompilingFailed;
+        }
+
+        if(verbose) printf("Compiled file '%s'.", "testcode.ansll");
+
+    CompilingFailed:
+        free(tokens);
+        free(strings);
+    LexingFailed:
+        
+        if(ret) return ret;
     }
 
     printf("Compilation succeeded.");
