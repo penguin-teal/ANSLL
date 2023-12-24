11621d5d2642e75db2e8eb2892296768e471fd77
penguin-teal <_penguin@tuta.io>
2023-12-23 18:21:26 -0600

    Test name mangling with no parameters

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "mangleName.h"
 
 int main(void)
 {
+    char name[256];
+    size_t size = mangleFunction(name, sizeof name, "std::namespace", "function", NULL, 0);
+    if(size > sizeof name || strcmp(name, "_AF$std?namespace$function$P$"))
+    {
+        fprintf(stderr, "Function 'std::namespace::function()' mangled to '%s' (%lu chars).\n", name, size);
+    }
+
+    size = mangleFunction(name, sizeof name, "unicode", "greek\xCF\x91THETA", NULL, 0);
+    if(size > sizeof name || strcmp(name, "_AF$unicode$greek@33D1THETA$P$"))
+    {
+        fprintf(stderr, "Function 'unicode::greek\xCF\x91THETA()' mangled to '%s' (%lu chars).\n", name, size);
+    }
+
     return 0;
 }
 
